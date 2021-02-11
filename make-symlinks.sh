88854f5be29b4c18170f249110f90df0cfac5f48
holger.hartmann <hhartmann@gmx.com>
2021-02-11 21:41:12 +0100

    updated

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -7,10 +7,10 @@
 ########## Variables
 
 # dotfiles directory
-dir=~/Users/Holger/dotfiles/
+dir=~/github/dotfiles/
 
 # old dotfiles backup directory
-olddir=~/Users/Holger/dotfiles/dotfiles_old
+olddir=~/github/dotfiles/dotfiles_old
 
 # list of files/folders to symlink in homedir
 files="bashrc zshrc vimrc"
@@ -30,7 +30,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir 
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
